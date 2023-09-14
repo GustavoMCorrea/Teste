8de9edfa5fcd12cc044458d310eb22e534cec7a6
rayan <carvalhorayane779@gmail.com>
2023-09-13 21:57:00 -0300

    Inclusão dos testes do sexo feminino

---
+++ src/test/java/org/example/ImcTest.java
@@ -0,0 +1,54 @@
+package org.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ImcTest {
+
+    Imc imc;
+
+    @BeforeEach
+    void setUp() {
+        imc = new Imc();
+    }
+
+    @Test
+    void deveRetornarFemininoAbaixoPeso(){
+        imc.setPeso(48.89);
+        imc.setAltura(1.60);
+        imc.setSexo("Feminino");
+        assertEquals("Abaixo do peso", imc.calcularImc());
+    }
+    @Test
+    void deveRetornarFemininoPesoNormal(){
+        imc.setPeso(66.04);
+        imc.setAltura(1.60);
+        imc.setSexo("Feminino");
+        assertEquals("No peso normal", imc.calcularImc());
+    }
+
+    @Test
+    void deveRetornarFemininoMarginalmenteAcimaDoPeso(){
+        imc.setPeso(69.88);
+        imc.setAltura(1.60);
+        imc.setSexo("Feminino");
+        assertEquals("Marginalmente acima do peso", imc.calcularImc());
+    }
+
+    @Test
+    void deveRetornarFemininoAcimaDoPesoIdeal(){
+        imc.setPeso(82.42);
+        imc.setAltura(1.60);
+        imc.setSexo("Feminino");
+        assertEquals("Acima do peso ideal", imc.calcularImc());
+    }
+
+    @Test
+    void deveRetornarFemininoObeso(){
+        imc.setPeso(82.69);
+        imc.setAltura(1.60);
+        imc.setSexo("Feminino");
+        assertEquals("Obeso", imc.calcularImc());
+    }
+}
